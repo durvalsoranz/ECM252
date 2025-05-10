a1d5c50bc474ed2d9f95a26b7bc397d256ee3fc4
durval <durvalsoranz@gmail.com>
2025-05-10 12:56:25 -0300

    learn(07-react): encerra a apostila
    -retomar fazendo o back end entre front e API Pexels a fim de isolar a
    chave

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -1,31 +1,44 @@
-import { createClient } from 'pexels'
+//import { createClient } from 'pexels'
+import pexelsClient from './utils/pexelsClient'
 import env from 'react-dotenv'
 import React from "react"
 import Busca from "./components/Busca"
+import ListaImagens from "./components/ListaImagens"
+import PexelsLogo from "./components/PexelsLogo"
 export default class App extends React.Component {
 
-  pexelsClient = null
+  // pexelsClient = null
 
   state = {
     photos: []
   }
 
   onBuscaRealizada = (termo) => {
-    this.pexelsClient.photos.search({
-      query: termo
+    pexelsClient.get('/search', {
+      params: { query: termo }
     })
-    .then(result => this.setState({photos: result.photos}))
+      .then(result => this.setState({ photos: result.data.photos }))
   }
 
-  componentDidMount() {
-    console.log(env.PEXELS_KEY)
-    this.pexelsClient = createClient(env.PEXELS_KEY)
-  }
+  // onBuscaRealizada = (termo) => {
+  //   this.pexelsClient.photos.search({
+  //     query: termo
+  //   })
+  //     .then(result => this.setState({ photos: result.photos }))
+  // }
+
+  // componentDidMount() {
+  //   console.log(env.PEXELS_KEY)
+  //   this.pexelsClient = createClient(env.PEXELS_KEY)
+  // }
 
   render() {
     return (
 
-      <div className="grid justify-content-center w-9 m-auto border-round border-1 border-200" >
+      <div className="grid justify-content-center w-9 m-auto border-round border-1 border-200">
+        <div className="col-12 text-center">
+          <PexelsLogo />
+        </div>
 
         <div className="col-12">
           <h4>Exibir uma lista de...</h4>
@@ -34,14 +47,13 @@ export default class App extends React.Component {
         <div className="col-12">
           <Busca onBuscaRealizada={this.onBuscaRealizada} />
         </div>
+
         <div className="col-12">
-          {
-            this.state.photos.map((photo, key) => (
-              <div>
-                key={key}
-              </div>
-            ))
-          }
+          <div className="grid">
+            <ListaImagens 
+            imgStyle={'md:col-6 lg:col-4 xl:col-3'} 
+            photos={this.state.photos} />
+          </div>
         </div>
       </div>
     )
